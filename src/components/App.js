a40786827a0ef8bfb87e2b83e6c43bbdb8341a34
blackpintz <rosewanjohi24@gmail.com>
2021-02-02 23:22:28 +0300

    update login form to receive data from the react controlled inputs.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import Login from './Login';
 
 const App = () => {
   useEffect(async () => {
-    const result = await axios.get('https://frozen-harbor-46293.herokuapp.com');
-    const { data } = result;
-    console.log(data);
+    if (localStorage.user) {
+      const result = await axios.get('https://frozen-harbor-46293.herokuapp.com/appointments', { headers: JSON.parse(localStorage.user) });
+      const { data } = result;
+      console.log(data);
+    } else {
+      console.log('Not logged in yet..');
+    }
   }, []);
   return (
-    <div className="App">
+    <div>
       <Login />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,76 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 class Login extends React.Component {
-    handleLogin = e => {
-      e.preventDefault();
-      const url = 'https://frozen-harbor-46293.herokuapp.com/auth/sign_in';
-      axios({
-        url,
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          Accept: 'application/json',
-        },
-        data: {
-          email: this.email.value,
-          password: this.password.value,
-        },
-      })
-        .then(response => {
-          console.log(response);
-        });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
 
-    render() {
-      return (
-        <div>
-          <h2>Log in</h2>
-          <form onSubmit={this.handleLogin}>
-            <input name="email" ref={input => this.email = input} />
-            <input name="password" type="password" ref={input => this.password = input} />
-            <input type="submit" value="Log in" />
-          </form>
-        </div>
-      );
-    }
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleLogin = e => {
+    const { email, password } = this.state;
+    e.preventDefault();
+    const url = 'https://frozen-harbor-46293.herokuapp.com/auth/sign_in';
+    axios({
+      url,
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        Accept: 'application/json',
+      },
+      data: {
+        email,
+        password,
+      },
+    }).then(response => {
+      console.log(response);
+      localStorage.setItem('user',
+        JSON.stringify({
+          'access-token': response.headers['access-token'],
+          client: response.headers.client,
+          uid: response.data.data.uid,
+        }));
+      window.location = '/';
+    }).catch(error => {
+      console.log(`There is an ${error}.`);
+    });
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <>
+        <h3>Log in.</h3>
+        <Form onSubmit={this.handleLogin}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
+            <Form.Text className="text-muted">
+              We will never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" name="password" value={password} onChange={this.handleChange} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </>
+    );
+  }
 }
 
 export default Login;
