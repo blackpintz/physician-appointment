22736b6d44abb14f97c19a2c26464b59b102ce97
blackpintz <rosewanjohi24@gmail.com>
2021-02-02 23:46:14 +0300

    add sign out functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 import '../App.css';
 import Login from './Login';
 
 const App = () => {
+  const currentUser = () => {
+    const user = localStorage.getItem('user');
+    return (user);
+  };
+
+  console.log(currentUser());
+  const handleSignOut = e => {
+    e.preventDefault();
+    axios({
+      method: 'DELETE',
+      url: 'https://frozen-harbor-46293.herokuapp.com/auth/sign_out',
+      data: JSON.parse(localStorage.user),
+    })
+      .then(() => {
+        localStorage.removeItem('user');
+        window.location = '/';
+      });
+  };
+
   useEffect(async () => {
     if (localStorage.user) {
       const result = await axios.get('https://frozen-harbor-46293.herokuapp.com/appointments', { headers: JSON.parse(localStorage.user) });
@@ -17,6 +37,9 @@ const App = () => {
   return (
     <div>
       <Login />
+      {currentUser() ? (
+        <Button className="mt-4" variant="danger" onClick={handleSignOut}>Logout</Button>
+      ) : (<></>)}
     </div>
   );
 };
