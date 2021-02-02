8d5d18cec6c0d565da12f8f4fc02e5c23525bfa6
blackpintz <rosewanjohi24@gmail.com>
2021-02-02 12:51:16 +0300

    make a get request for the physicians data to the rails api.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
+import React, {useEffect} from 'react'
+import axios from 'axios';
 import '../App.css';
 import Login from './Login';
 
 
-function App() {
+const App = () => {
+  useEffect(async () => {
+    const result = await axios.get('https://frozen-harbor-46293.herokuapp.com');
+    const { data } = result;
+    console.log(data)
+  }, []);
   return (
     <div className="App">
     <Login />
