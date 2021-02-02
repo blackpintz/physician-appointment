9f0d510be47166142778f9eaaecc158896eaf6d5
blackpintz <rosewanjohi24@gmail.com>
2021-02-02 14:13:06 +0300

    set up github actions.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import '../App.css';
 import Login from './Login';
-
 
 const App = () => {
   useEffect(async () => {
     const result = await axios.get('https://frozen-harbor-46293.herokuapp.com');
     const { data } = result;
-    console.log(data)
+    console.log(data);
   }, []);
   return (
     <div className="App">
-    <Login />
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,39 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
-    handleLogin = (e) => {
-        e.preventDefault()
-        const url = 'https://frozen-harbor-46293.herokuapp.com/auth/sign_in';
-        axios({
-          url: url,  
-          method: 'POST',
-          headers: {
-            "content-type": "application/json",
-            "Accept": "application/json"
+    handleLogin = e => {
+      e.preventDefault();
+      const url = 'https://frozen-harbor-46293.herokuapp.com/auth/sign_in';
+      axios({
+        url,
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          Accept: 'application/json',
         },
-          data: {
-            email: this.email.value,
-            password: this.password.value
-          }
-        })
+        data: {
+          email: this.email.value,
+          password: this.password.value,
+        },
+      })
         .then(response => {
-          console.log(response)
-        })
-      }
-  render () {
-    return (
-      <div>
-        <h2>Log in</h2>
-        <form onSubmit={this.handleLogin} >
-          <input name="email" ref={(input) => this.email = input } />
-          <input name="password" type="password" ref={(input) => this.password = input } />
-          <input type="submit" value="Log in" />
-        </form>
-      </div>
-    )
-  }
+          console.log(response);
+        });
+    }
+
+    render() {
+      return (
+        <div>
+          <h2>Log in</h2>
+          <form onSubmit={this.handleLogin}>
+            <input name="email" ref={input => this.email = input} />
+            <input name="password" type="password" ref={input => this.password = input} />
+            <input type="submit" value="Log in" />
+          </form>
+        </div>
+      );
+    }
 }
 
-export default Login
+export default Login;
