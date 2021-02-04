9023ebd99802a0a979ae332e68ee107dcf1eb382
blackpintz <rosewanjohi24@gmail.com>
2021-02-04 22:23:20 +0300

    Finished styling the home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Card } from 'react-bootstrap';
 
 const Home = () => {
   const [data, setData] = useState([]);
   useEffect(async () => {
-    let isMounted = true;
     if (localStorage.user) {
       const result = await axios.get('https://frozen-harbor-46293.herokuapp.com/physicians', { headers: JSON.parse(localStorage.user) });
       const { data } = result;
-      if (isMounted) setData(data);
+      setData(data);
     } else {
       <p>Not logged in yet..</p>;
     }
-    return () => { isMounted = false; };
   }, []);
   return (
-    <>
-      <h3>See all your appointments here.</h3>
-      <h2>Available Physicians</h2>
-      <ul>
-        {data.map(item => (
-          <li key={item.id} className="h4">
-            {item.category}
+    <Container>
+      <h3 className="mt-2">Available Physicians</h3>
+      {data.map(item => (
+        <Card key={item.id} className="mb-2 w-50">
+          <Card.Body>
+            <Card.Title>{item.category}</Card.Title>
             <Link to={`/physician/${item.id}`}>
               <Button variant="info">Book an appointment</Button>
             </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+          </Card.Body>
+        </Card>
+      ))}
+    </Container>
   );
 };
 
