0ddd91a9ee43efc65a3231656d7d8598fe6b6e67
blackpintz <rosewanjohi24@gmail.com>
2021-02-05 17:24:26 +0300

    write tests for static components.

---
+++ src/_tests_/AppointmentList.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import AppointmentList from '../components/AppointmentList';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('should render AppointmentList component', () => {
+  const wrapper = shallow(<AppointmentList />);
+  expect(wrapper).toMatchSnapshot();
+});
---
+++ src/_tests_/Menu.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Menu from '../components/Menu';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('should render Menu component', () => {
+  const wrapper = shallow(<Menu />);
+  expect(wrapper).toMatchSnapshot();
+});
